47bd8dfcbf1f99555a8eaf8bf1d6d52d52bfedeb
my-0998 <68629457+my-0998@users.noreply.github.com>
2020-10-23 14:32:10 +0900

    js修正

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,48 @@
 $(function(){
+ 
+    let txt1 = "おはようございます！";
+    let txt2 = "おげんきですか？";
+    let num1 = 20;
+    let num2 = 32;
+    　　
+
+    function sayhello(){
+        $("#content").txet("こんにちは！");
+    }
+
+    function saysomething(txt){
+        $("#content").txet(txt);
+    }
+
+    function samtwo(txt){
+        $("#content").txet(txt)
+    }
 
 
+    $("#button01").on("click",function(){
+      seyhello();
+    })
+    
+    $("#button02").on("click",function(){
+      saysomething(txt1);
+    })
+    
+    $("#button03").on("click",function(){
+      somtwo(txt1,txt2)
+    })
+    
+    $("#button04").on("click",function(){
+        somtwo(num1,num2)
+    })
+    
+    
+    $("#button05").on("click",function(){
+        somtwo(txt1,txt2);
+        somtwo(num1,num2);
 
-
-})
+    })
+    
+    
+   })
+   
+   
