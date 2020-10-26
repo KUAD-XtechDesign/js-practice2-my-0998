fae734ab5147ad184e5d5b6ca3b278b90ed1d813
my-0998 <68629457+my-0998@users.noreply.github.com>
2020-10-27 00:21:33 +0900

    練習問題３修正

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,46 +1,45 @@
 $(function(){
  
-    let txt1 = "おはようございます！";
-    let txt2 = "おげんきですか？";
-    let num1 = 20;
-    let num2 = 32;
+  let txt1 = "おはようございます！";
+  let txt2 = "おげんきですか？";
+  let num1 = 20;
+  let num2 = 32;
     　　
 
-    function sayHello(){
-      $("#content").txet("こんにちは！");
-    }
+  function sayHello(){
+    $("#content").txet("こんにちは！");
+  }
 
-    function saySomething(txt){
-      $("#content").txet(txt);
-    }
+  function saySomething(txt){
+    $("#content").txet(txt);
+  }
 
-    function samTwo(txt){
-      $("#content").txet(txt)
-    }
+  function samTwo(txt){
+    $("#content").txet(txt);
+  }
 
 
-    $("#button01").on("click",function(){
-      sayHello();
-    })
+  $("#button01").on("click",function(){
+    sayHello();
+  })
     
-    $("#button02").on("click",function(){
-      saySomething(txt1);
-    })
+  $("#button02").on("click",function(){
+    saySomething(txt1);
+  })
     
-    $("#button03").on("click",function(){
-      samTwo(txt1,txt2)
-    })
+  $("#button03").on("click",function(){
+    samTwo(txt1,txt2)
+  })
     
-    $("#button04").on("click",function(){
-        samTwo(num1,num2)
-    })
+  $("#button04").on("click",function(){
+    samTwo(num1,num2)
+  })
     
     
-    $("#button05").on("click",function(){
-        samTwo(txt1,txt2)
-        samTwo(num1,num2)
-
-    })
+  $("#button05").on("click",function(){
+    samTwo(txt1,txt2)
+    samTwo(num1,num2)
+  })
     
     
 })
